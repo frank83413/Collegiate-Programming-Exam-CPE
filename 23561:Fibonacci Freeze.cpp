9d9251aef2908e04d02dd34c242d96a39ef1fb6a
frank83413 <frank83413@gmail.com>
2018-04-10 14:09:24 +0800

    Update 23561:Fibonacci Freeze.cpp

--- 23561:Fibonacci Freeze.cpp
+++ 23561:Fibonacci Freeze.cpp
@@ -11,18 +11,18 @@ long long int F[5001][size] = { 0 };
 void Fibonacci(long long int F[5001][size]) {
 	F[1][0] = 1;
 	for (int i = 2; i < 5001; i++) {
-		for (int j=0; j < size; j++) {
+		for (int j = 0; j < size; j++) {
 			F[i][j] += F[i - 1][j] + F[i - 2][j];
 			if (F[i][j] > 10000000000) {
-				F[i][j] -= 10000000000;
-				F[i][j + 1]++;
+				F[i][j + 1]+= F[i][j] / 10000000000;
+				F[i][j] -= F[i][j + 1] * 10000000000;
 			}
 		}
 	}
 }
 
 int main() {
-	int in;	
+	int in;
 	Fibonacci(F);
 	while (cin >> in) {
 		if (in == 0) {
@@ -34,13 +34,13 @@ int main() {
 			continue;
 		}
 		cout << "The Fibonacci number for " << in << " is ";
-		int count = size-1;
+		int count = size - 1;
 		while (F[in][count] == 0) {
 			count--;
 		}
 		cout << F[in][count];
 		count--;
-		for (int i= count;i>-1;i--)
+		for (int i = count; i>-1; i--)
 		{
 			cout << setw(10) << setfill('0') << F[in][i];
 		}
